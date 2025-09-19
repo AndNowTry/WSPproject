3955b11c20a51e1f7d0597c5a119d7096f72587c
AND_NOW <borovandrew@gmail.com>
2025-09-19 09:19:54 +0300

    Тестил базу

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    data_for_temple = {}
+    return render(request, 'main_app/home_page.html', data_for_temple)
